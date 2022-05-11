2abfa44344e0c93943279906fa61ae46bd7e3d41
Samyak Shah <samyakshah3008@gmail.com>
2022-05-11 15:18:03 +0530

    Router path add to each page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
+import { Routes, Route, Link } from "react-router-dom";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+      </Routes>
     </div>
   );
 }
