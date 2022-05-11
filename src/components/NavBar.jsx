cfdaec720abfda9a42e8d896e5112bf286ba03e7
Samyak Shah <samyakshah3008@gmail.com>
2022-05-11 22:56:00 +0530

    FEAT: Navbar component add

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import { Box, Heading } from "@chakra-ui/react";
+import React from "react";
+import MainContainer from "./MainContainer";
+
+export default function NavBar() {
+  return (
+    <div>
+      <nav>
+        <Box
+          p={3}
+          justifyContent="space-between"
+          alignItems="center"
+          display="flex"
+          height="80px"
+        >
+          <Heading
+            marginLeft={20}
+            as="h4"
+            color="purple.600"
+            fontWeight="extrabold"
+            size="md"
+          >
+            TripleS-Chat
+          </Heading>
+        </Box>
+      </nav>
+
+      <MainContainer />
+    </div>
+  );
+}
