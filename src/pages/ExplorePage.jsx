53cf6156452c3800fba0e48d0858b85dc34aa2a9
Samyak Shah <samyakshah3008@gmail.com>
2022-05-11 23:25:19 +0530

    FEAT: explore page initial commit

---
+++ src/pages/ExplorePage.jsx
@@ -0,0 +1,19 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import BookmarkPostContainer from "../components/BookmarkPostContainer";
+import NavBar from "../components/NavBar";
+import RightSideBar from "../components/RightSideBar";
+import SideBar from "../components/SideBar";
+
+export default function ExplorePage() {
+  return (
+    <div>
+      <NavBar />
+      <Flex bg="#E5E5E5">
+        <SideBar />
+        <BookmarkPostContainer />
+        <RightSideBar />
+      </Flex>
+    </div>
+  );
+}
