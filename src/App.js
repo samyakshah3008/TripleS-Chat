89e4f5c8c9146195f7351567465456382aad1312
Samyak Shah <samyakshah3008@gmail.com>
2022-05-11 13:19:59 +0530

    Refactor: minor update, moved components to single page component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import { Container, Flex } from "@chakra-ui/react";
 import "./App.css";
-import Footer from "./components/Footer";
-import HeroHeader from "./components/HeroHeader";
-import LoginContainer from "./components/LoginContainer";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <div className="App">
-      <Container marginTop={30} maxW="container.lg" p={0}>
-        <Flex
-          gap={5}
-          justifyContent="center"
-          width="80%"
-          margin="auto"
-          h="75vh"
-        >
-          <HeroHeader />
-          <LoginContainer />
-        </Flex>
-        <Footer />
-      </Container>
+      <LoginPage />
     </div>
   );
 }
