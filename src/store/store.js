ad218e843357bdff072d5133fb9884145b8e3023
Samyak Shah <samyakshah3008@gmail.com>
2022-07-18 23:58:47 +0530

    store updated

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./authSlice";
+import userReducer from "./userSlice";
+import postReducer from "./postSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    users: userReducer,
+    posts: postReducer,
   },
 });
 
