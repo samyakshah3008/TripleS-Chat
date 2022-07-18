0a24f6b940bffab1a659638a2e632482b1dc7f3e
Samyak Shah <samyakshah3008@gmail.com>
2022-07-18 23:58:02 +0530

    userpost component update

--- src/components/UserPost.jsx
+++ src/components/UserPost.jsx
@@ -4,42 +4,62 @@ import * as AiIcons from "react-icons/ai";
 import * as BsIcons from "react-icons/bs";
 import * as VsIcons from "react-icons/vsc";
 import * as SiIcons from "react-icons/si";
+import { useSelector } from "react-redux";
 
 export default function UserPost() {
+  const { posts } = useSelector((state) => state.posts);
+  const { users } = useSelector((state) => state.users);
+  const { userInfo } = useSelector((state) => state.auth);
+
+  const loggedUser = users?.find(
+    (eachUser) => eachUser.username === userInfo.username
+  );
+
+  const loggedUserFollowing = loggedUser?.following;
+
+  const feedPost = posts.filter(
+    (eachPost) =>
+      loggedUserFollowing?.find(
+        (each) => each.username === eachPost.username
+      ) || eachPost.username === loggedUser?.username
+  );
+
   return (
     <div>
-      <Box h="auto" p={5} marginTop="20px" bg="white">
-        <Flex gap={5}>
-          <Avatar
-            size="md"
-            name="Ryan Florence"
-            src="https://bit.ly/ryan-florence"
-          />
-          <Flex
-            justifyContent="space-between"
-            gap={4}
-            flexGrow={1}
-            flexDirection="column"
-          >
-            <Flex gap={2} alignItems="center">
-              <Heading size="sm">Samyak Shah</Heading>
-              <Text color="gray.500">@SamyakShah</Text>
-            </Flex>
+      {feedPost?.map((post) => {
+        return (
+          <>
+            <Box h="auto" p={5} marginTop="20px" bg="white">
+              <Flex gap={5}>
+                <Avatar size="md" name="Ryan Florence" src={post.avatar} />
+                <Flex
+                  justifyContent="space-between"
+                  gap={4}
+                  flexGrow={1}
+                  flexDirection="column"
+                >
+                  <Flex gap={2} alignItems="center">
+                    <Heading size="sm"> {post.username} </Heading>
+                    <Text color="gray.500">@ {post.username} </Text>
+                  </Flex>
 
-            <Text>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci
-              esse voluptate explicabo, debitis minus earum!
-            </Text>
-            <Flex justifyContent="space-between" fontSize="xl" gap={10}>
-              <AiIcons.AiOutlineHeart cursor="pointer" />
-              <VsIcons.VscComment cursor="pointer" />
-              <AiIcons.AiOutlineShareAlt cursor="pointer" />
-              <BsIcons.BsBookmarkStar cursor="pointer" />
-              <SiIcons.SiSimpleanalytics cursor="pointer" />
-            </Flex>
-          </Flex>
-        </Flex>
-      </Box>
+                  <Text>{post.content}</Text>
+                  <Flex justifyContent="space-between" fontSize="xl" gap={10}>
+                    <Flex gap={3} alignItems="center">
+                      <AiIcons.AiOutlineHeart cursor="pointer" />
+                      <Box fontSize={15}> {post.likes.likeCount} </Box>
+                    </Flex>
+                    <VsIcons.VscComment cursor="pointer" />
+                    <AiIcons.AiOutlineShareAlt cursor="pointer" />
+                    <BsIcons.BsBookmarkStar cursor="pointer" />
+                    <SiIcons.SiSimpleanalytics cursor="pointer" />
+                  </Flex>
+                </Flex>
+              </Flex>
+            </Box>
+          </>
+        );
+      })}
     </div>
   );
 }
