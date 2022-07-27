248be2c5d033b4222d93024cca27f2b4379dea88
Samyak Shah <samyakshah3008@gmail.com>
2022-07-27 22:30:57 +0530

    minor update

--- src/backend/controllers/CommentsController.js
+++ src/backend/controllers/CommentsController.js
@@ -129,6 +129,7 @@ export const editPostCommentHandler = function (schema, request) {
 
 export const deletePostCommentHandler = function (schema, request) {
   const user = requiresAuth.call(this, request);
+
   try {
     if (!user) {
       return new Response(
