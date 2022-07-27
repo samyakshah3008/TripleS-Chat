63e7bece2ca052ec4e388d338d4acf026c2b1fae
Samyak Shah <samyakshah3008@gmail.com>
2022-07-27 18:16:45 +0530

    userpost minor update

--- src/components/UserPost.jsx
+++ src/components/UserPost.jsx
@@ -1,5 +1,22 @@
-import { Avatar, Box, Flex, Heading, Text } from "@chakra-ui/react";
-import React from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  Textarea,
+  useDisclosure,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 import * as AiIcons from "react-icons/ai";
 import * as BsIcons from "react-icons/bs";
 import * as VsIcons from "react-icons/vsc";
@@ -9,27 +26,17 @@ import {
   bookmarkPosts,
   dislikePosts,
   likePosts,
+  postComments,
   removeBookmarkPosts,
 } from "../store/postSlice";
+import Comments from "./Comments";
+import CommentModal from "./CommentModal";
 
-export default function UserPost() {
+export default function UserPost({ post }) {
   const { posts, bookmarks } = useSelector((state) => state.posts);
   const { users } = useSelector((state) => state.users);
   const { userInfo, token } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
-
-  const loggedUser = users?.find(
-    (eachUser) => eachUser.username === userInfo.username
-  );
-
-  const loggedUserFollowing = loggedUser?.following;
-
-  const feedPost = posts.filter(
-    (eachPost) =>
-      loggedUserFollowing?.find(
-        (each) => each.username === eachPost.username
-      ) || eachPost.username === loggedUser?.username
-  );
 
   const bookmarkByUser = (selectedPostId) => {
     return (
@@ -62,66 +69,109 @@ export default function UserPost() {
     }
   };
 
+  const { onOpen, onClose, isOpen } = useDisclosure();
+
+  const commentTextHandler = (e) => {
+    setCommentData(e.target.value);
+  };
+
+  const [commentData, setCommentData] = useState("");
+
+  const createNewCommentHandler = (post) => {
+    dispatch(
+      postComments({
+        postId: post._id,
+        commentData: commentData,
+        token: token,
+      })
+    );
+    setCommentData("");
+
+    onClose();
+  };
+
   return (
-    <div>
-      {feedPost?.map((post) => {
-        return (
-          <>
-            <Box h="auto" p={5} marginTop="20px" bg="white">
-              <Flex gap={5}>
-                <Avatar size="md" name="Ryan Florence" src={post.avatar} />
-                <Flex
-                  justifyContent="space-between"
-                  gap={4}
-                  flexGrow={1}
-                  flexDirection="column"
-                >
-                  <Flex gap={2} alignItems="center">
-                    <Heading size="sm"> {post.username} </Heading>
-                    <Text color="gray.500">@ {post.username} </Text>
-                  </Flex>
+    <div key={post._id}>
+      <Box h="auto" p={5} marginTop="20px" bg="white">
+        <Flex gap={5}>
+          <Avatar size="md" name="Ryan Florence" src={post.avatar} />
+          <Flex
+            justifyContent="space-between"
+            gap={4}
+            flexGrow={1}
+            flexDirection="column"
+          >
+            <Flex gap={2} alignItems="center">
+              <Heading size="sm"> {post.username} </Heading>
+              <Text color="gray.500">@ {post.username} </Text>
+            </Flex>
+            <Text>{post.content}</Text>
+            <Flex justifyContent="space-between" fontSize="xl" gap={10}>
+              <Flex gap={3} alignItems="center">
+                {likeByUser(post) ? (
+                  <AiIcons.AiFillHeart
+                    bg="red"
+                    onClick={() => likeHandler(post)}
+                    cursor="pointer"
+                  />
+                ) : (
+                  <AiIcons.AiOutlineHeart
+                    onClick={() => likeHandler(post)}
+                    cursor="pointer"
+                  />
+                )}
 
-                  <Text>{post.content}</Text>
-                  <Flex justifyContent="space-between" fontSize="xl" gap={10}>
-                    <Flex gap={3} alignItems="center">
-                      {likeByUser(post) ? (
-                        <AiIcons.AiFillHeart
-                          bg="red"
-                          onClick={() => likeHandler(post)}
-                          cursor="pointer"
-                        />
-                      ) : (
-                        <AiIcons.AiOutlineHeart
-                          onClick={() => likeHandler(post)}
-                          cursor="pointer"
-                        />
-                      )}
-
-                      <Box fontSize={15}> {post.likes.likeCount} </Box>
-                    </Flex>
-                    <VsIcons.VscComment cursor="pointer" />
-                    <AiIcons.AiOutlineShareAlt cursor="pointer" />
-
-                    {bookmarkByUser(post._id) ? (
-                      <BsIcons.BsFillBookmarkCheckFill
-                        onClick={() => bookmarkHandler(post._id)}
-                        cursor="pointer"
-                      />
-                    ) : (
-                      <BsIcons.BsBookmarkStar
-                        onClick={() => bookmarkHandler(post._id)}
-                        cursor="pointer"
-                      />
-                    )}
-
-                    <SiIcons.SiSimpleanalytics cursor="pointer" />
-                  </Flex>
-                </Flex>
+                <Box fontSize={15}> {post.likes.likeCount} </Box>
               </Flex>
-            </Box>
-          </>
-        );
-      })}
+              <VsIcons.VscComment onClick={onOpen} cursor="pointer" />
+              <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                  <ModalHeader>Add comment</ModalHeader>
+
+                  <ModalCloseButton />
+                  <ModalBody>
+                    <Textarea
+                      onChange={commentTextHandler}
+                      _focus={{
+                        border: "2px",
+                        borderColor: "purple.600",
+                      }}
+                      placeholder="Start Typing...."
+                    />
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button
+                      colorScheme="purple"
+                      mr={3}
+                      onClick={() => createNewCommentHandler(post)}
+                    >
+                      Comment
+                    </Button>
+                  </ModalFooter>
+                </ModalContent>
+              </Modal>
+
+              <AiIcons.AiOutlineShareAlt cursor="pointer" />
+
+              {bookmarkByUser(post._id) ? (
+                <BsIcons.BsFillBookmarkCheckFill
+                  onClick={() => bookmarkHandler(post._id)}
+                  cursor="pointer"
+                />
+              ) : (
+                <BsIcons.BsBookmarkStar
+                  onClick={() => bookmarkHandler(post._id)}
+                  cursor="pointer"
+                />
+              )}
+
+              <SiIcons.SiSimpleanalytics cursor="pointer" />
+            </Flex>
+          </Flex>
+        </Flex>
+        <Comments eachPost={post} />
+      </Box>
     </div>
   );
 }
