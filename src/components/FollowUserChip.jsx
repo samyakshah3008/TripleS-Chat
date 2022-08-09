290bf4e2c8db97567aa7ffe114da0019c2cb9271
Samyak Shah <samyakshah3008@gmail.com>
2022-08-10 03:06:00 +0530

    Minor refactoring

--- src/components/FollowUserChip.jsx
+++ src/components/FollowUserChip.jsx
@@ -2,11 +2,14 @@ import { Avatar, Flex, Heading, Text } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { followUser, getAny } from "../store/userSlice";
+import { getAny } from "../store/userSlice";
+import { useNavigate } from "react-router-dom";
+import { followUser, unfollowUser } from "../store/authSlice";
 
 export default function FollowUserChip() {
   const [dataBase, setDataBase] = useState({});
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const { users } = useSelector((state) => state.users);
 
@@ -25,29 +28,38 @@ export default function FollowUserChip() {
     loadData();
   }, []);
 
-  const currentUser = users?.find(
-    (each) => each?.username === userInfo?.username
+  // const currentUser = users?.find(
+  //   (each) => each?.username === userInfo?.username
+  // );
+
+  const allUsers = users?.filter(
+    (eachUser) => eachUser.username !== userInfo.username
   );
 
-  const currentUserFollowing = currentUser?.following;
+  // const currentUserFollowing = currentUser?.following;
 
-  const userSuggestion = users?.filter(
-    (eachUser) =>
-      !currentUserFollowing?.find(
-        (each) => eachUser.username === each.username
-      ) && eachUser.username !== currentUser?.username
-  );
+  // const userSuggestion = users?.filter(
+  //   (eachUser) =>
+  //     !currentUserFollowing?.find(
+  //       (each) => eachUser.username === each.username
+  //     ) && eachUser.username !== currentUser?.username
+  // );
 
   // !currentUserFollowing.find will be having those names which are not followed.
-  // second condition excludes self user account.
+  // second condition excludes self user account. userInfo.following find item.username
 
   return (
     <div>
-      {userSuggestion?.map((item) => {
+      {allUsers?.map((item) => {
         return (
           <div key={item._id}>
             <Flex marginBottom={4} w={260} gap={4} cursor="pointer">
-              <Avatar size="md" name="Ryan Florence" src={item.avatar} />
+              <Avatar
+                onClick={() => navigate(`/profile/${item.username}`)}
+                size="md"
+                name="Ryan Florence"
+                src={item.avatar}
+              />
               <Flex justifyContent="flex-start" flexDirection="column">
                 <Heading size="sm"> {item.firstName} </Heading>
                 <Text color="gray.500" fontSize="sm">
@@ -61,17 +73,37 @@ export default function FollowUserChip() {
                 gap={10}
                 cursor="pointer"
               >
-                <Text
-                  onClick={() =>
-                    dispatch(
-                      followUser({ followUserId: item._id, token: token })
-                    )
-                  }
-                  fontWeight="bold"
-                  color="purple.600"
-                >
-                  Follow +
-                </Text>
+                {userInfo.following.find(
+                  (userInfo) => userInfo.username === item.username
+                ) ? (
+                  <>
+                    <Text
+                      onClick={() =>
+                        dispatch(
+                          unfollowUser({ followUserId: item._id, token: token })
+                        )
+                      }
+                      fontWeight="bold"
+                      color="purple.600"
+                    >
+                      UnFollow
+                    </Text>
+                  </>
+                ) : (
+                  <>
+                    <Text
+                      onClick={() =>
+                        dispatch(
+                          followUser({ followUserId: item._id, token: token })
+                        )
+                      }
+                      fontWeight="bold"
+                      color="purple.600"
+                    >
+                      Follow +
+                    </Text>
+                  </>
+                )}
               </Flex>
             </Flex>
           </div>
